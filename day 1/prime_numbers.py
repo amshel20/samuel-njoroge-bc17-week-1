5381d74c0f7b1b9acab597b316aabbb1ef082c70
Amshel Kanyi <amshel20@gmail.com>
2017-04-04 07:32:07 +0300

    changed function name

--- day 1/prime_numbers.py
+++ day 1/prime_numbers.py
@@ -1,9 +1,9 @@
 '''A working function to generate prime numbers from 0 to n with asymptotic analysis in your public Github repo'''
-def is_prime(N):
-    for num in range(2,N+ 1):
+def list_primes(N):
+    for num in range(0,N+1):
        if num > 1:
            for i in range(2,num):
                if num % i == 0:
                    break
            else:
-               return num
+               print (num)
